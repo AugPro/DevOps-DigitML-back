7faf056db068bce89332fdcca66c28a3e7b1976d
Augustin Prolongeau <augustin.pro@gmail.com>
2019-11-19 11:30:31 +0100

    Added CORS Modified front and back to show image result

--- DigitML/settings.py
+++ DigitML/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS
+
+CORS_ORIGIN_WHITELIST = [
+    "file://",
+    "null",
+]
